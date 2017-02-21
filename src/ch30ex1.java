f98317a87a79bb86b5355de1c10fc4fad4eaeb14
William Patrick O Brien <obrienwp@dcsdk12.org>
2017-02-21 12:52:43 -0700

    Create java project and complete ch30ex1

---
+++ src/ch30ex1.java
@@ -0,0 +1,29 @@
+
+//public class ch30ex1 {
+
+		class HelloObject
+		{
+			String greeting;
+			HelloObject(String st){
+				greeting = st;
+			}
+			
+			void speak()
+			{
+				for (int i = 1; i <= greeting.length(); i++){
+				System.out.println(greeting);
+				}
+			}
+		}
+		
+		class HelloTester
+		{
+			public static void main(String[] args)
+			{
+				HelloObject anObject = new HelloObject("Hello");
+				anObject.speak();
+			}
+		}
+	//}
+
+
