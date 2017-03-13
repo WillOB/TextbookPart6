d53126c9d0b27347ccb63ea0f180be2288ad6b21
WillOB <obrienwp@s.dcsdk12.org>
2017-03-13 17:16:57 -0600

    Finish chapter 33

--- src/chapter33/Box.java
+++ src/chapter33/Box.java
@@ -2,9 +2,9 @@ package chapter33;
 
 public class Box {
 	
-	public double width;
-	public double height;
-	public double length;
+	private double width;
+	private double height;
+	private double length;
 	
 	Box(double width, double height, double length){
 		this.width = width;
@@ -16,6 +16,12 @@ public class Box {
 		this.width = this.height = this.length = side;
 	}
 	
+	Box(Box oldBox){
+		this.width = oldBox.width;
+		this.height = oldBox.height;
+		this.length = oldBox.length;
+	}
+	
 
 	public double volume(){
 		return width * height * length;
@@ -24,5 +30,45 @@ public class Box {
 	public double area(){
 		return 2 * width * height + 2 * width * length + 2 * height * length;
 	}
+	
+	private double faceArea(){
+		return width * height;
+	}
+	
+	private double topArea(){
+		return width * length;
+	}
+	
+	private double sideArea(){
+		return height * length;
+	}
+	
+	public double area2(){
+		return 2 * faceArea() + 2 * topArea() + 2 * sideArea();
+	}
+	
+	public double length(){
+		return length;
+	}
+	
+	public double height(){
+		return height;
+	}
+	
+	public double width(){
+		return width;
+	}
+	
+	public Box biggerBox(Box oldBox){
+		return new Box(1.25 * oldBox.width, 1.25 * oldBox.height, 1.25 * oldBox.length);
+	}
 
+	public Box smallerBox(Box oldBox){
+		return new Box(.75 * oldBox.width, .75 * oldBox.height, .75 * oldBox.length);
+	}
+	
+	public boolean nests(Box outsideBox){
+		return width < outsideBox.width && height < outsideBox.height && 
+				length < outsideBox.length;
+	}
 }
--- src/chapter33/BoxTester.java
+++ src/chapter33/BoxTester.java
@@ -7,10 +7,46 @@ class BoxTester
   {
      Box box = new  Box( 2.5, 5.0, 6.0 ) ;
 
+     //exercise 1
      System.out.println( "Area: "  + box.area() + " volume: " + box. volume() );
 
-     System.out.println( "length: " + box.length + " height: " + box. height +
-                         " width:  " + box.width )  ;
+     //System.out.println( "length: " + box.length + " height: " + box. height +
+     //                    " width:  " + box.width )  ;
      //that part won't compile because length, height, and width are private variables
+     //I changed them to public so the tester class can see them
+     
+     //exercise 2
+     System.out.println("Area: " + box.area2());
+     //System.out.println("Top area: " + box.topArea());
+     //This line won't compile because topArea() is private
+     
+     //exercise 3
+     System.out.println( "length: " + box.length() + " height: " + box. height() +
+                         " width:  " + box.width() )  ;
+     Box box2 = new Box(2.5, 5.0, 6.0);
+     System.out.println("New box area: " + box2.area());
+     
+     //exercise 4
+     Box bigBox = box.biggerBox(box);
+     System.out.println("Bigger box width: " + bigBox.width() + " height: " + bigBox.height()
+     	+ " length: " + bigBox.length());
+     Box smallBox = box.smallerBox(box);
+     System.out.println("Smaller box width: " + smallBox.width() + " height: " + smallBox.height()
+    		 + " length: " + smallBox.length());
+     
+     //exercise 5
+     if(box.nests(bigBox))
+    	 System.out.println("box fits inside bigBox");
+     else
+    	 System.out.println("Something is wrong");
+     if(smallBox.nests(box))
+    	 System.out.println("smallBox fits inside box");
+     else
+    	 System.out.println("Something is wrong");
+     Box longBox = new Box(2.0, 2.0, 200.0);
+     if(box.nests(longBox))
+    	 System.out.println("Something is wrong");
+     else
+    	 System.out.println("box does not fit in longBox");
   }
 }
